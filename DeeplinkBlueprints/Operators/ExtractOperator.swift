f4549a7c2b11f431f438400248fb433eea002a70
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-04-09 02:08:57 +0200

    operators

---
+++ DeeplinkBlueprints/Operators/ExtractOperator.swift
@@ -0,0 +1,9 @@
+//
+//  ExtractOperator.swift
+//  DeeplinkBlueprints
+//
+//  Created by Tomasz Lewandowski on 09/04/2020.
+//  Copyright © 2020 LionSoftware.org. All rights reserved.
+//
+
+infix operator ~>
